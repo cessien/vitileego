f64ba5e4e6baac1c02278f542c3608f90593cc1d
Charles <cessien88@gmail.com>
2014-10-12 16:45:14 -0400

    still no luck pushing byte data to pnacl module

--- vitileegocv.cc
+++ vitileegocv.cc
@@ -38,9 +38,10 @@ class VitileegoCVEngine : public pp::Instance {
 			} else if (fn == "open") { //open a specific file
 				open(messageJSON.Get("file").AsString());
 			} else if (fn == "picture") { //decode the picture
-				decode(messageJSON.Get("picture").AsString(),messageJSON.Get("size").AsInt());
+				decode(messageJSON.Get("picture"),messageJSON.Get("size").AsInt());
 			}
 			
+
 		} else if (message.is_string()) { //Simple dummy test to respond
 			char tmessage[1024];
 			sprintf(tmessage,"%s %s",message.AsString().c_str(), "! Hi there :)");
@@ -50,10 +51,7 @@ class VitileegoCVEngine : public pp::Instance {
 			PostMessage(reply);
 		} else { //Assume possible image_data
 			
-		}
-		
-
-		
+		}	
 	}
 	
 	void init() {
@@ -88,20 +86,42 @@ class VitileegoCVEngine : public pp::Instance {
 	 */
 	void open(std::string file) {
 	}
-	
-	void decode(const std::string & pictureData,int size) {
-		const std::string code64 = pictureData;			
-		//const std::string code64("iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAp1JREFUeNqEU21IU1EYfu7unW5Ty6aBszYs6MeUjGVYokHYyH5E1B9rZWFEFPQnAwmy6Hc/oqhfJsRKSSZGH1JIIX3MNCsqLTD9o1Oj6ebnnDfvvefezrnbdCHhCw/n433P8z7nPe/hBEEAtX0U7hc164uwuvVSXKwZLoOmaRDim+7m9vZa0WiEKSUFFpNpCWlmMyypqTDRuYn6t3k8vmQ2gRDCxs0t9fW45F52aBTROJLtZl7nEZad2m+KtoQCQ0FBARyOCGRZ/q92I1WgqqXlfdd95VsrK8/pChIEqqpCkiQsiCII0aBQZZoWl8lzFDwsFjMl0DBLY8Lj41hBwK4jSQrWOIphL6xYyhwJDWGo6wFSaH1Y3PTCAsITE1oyAa8flhWkbSiCLX8vun11eiGIpiJ/z2nYdx5HqLdVV7elrOzsuqysL3rmBIGiKPizKCHHWY4PLVeQbnXAdegqdhy+hu8dDTBnbqQJZJ1A7u+vz7RaiymWCZgCRSF6Edk8b9cx+B/W6WuVxPaZnyiqXoPpyUmVYvkKTIFClHigEieKjYuSvETUllaF4GAUM1NT6ooaJDKx+aDfC9fByxj90REb+9ppmIoAscH/6leg8MS9DJXPAM9xHCM443K57C6biMjcHDaVVCHw9RmCA2/RGC5C00AqXk/m4p20HZK4CM/J3Zk9n0ecMBhDQnJHcrTisyMfdQXOilrdMfxcwoHq/fg5R59TiQV3hYGKo6X2J/c7LyQIjOx9GXhOw/zoJ8wEevRGyp53o/lGMNYsBgPtEwLecwov7/jGDKa1twT6o3KpL4MdZgGsWZLtfPr7f1q58k1JNHy7YYaM+J+K3Y2PmAIbRavX66229hrGVvvL5uzsHDEUvUu+NT1my78CDAAMK1a8/QaZCgAAAABJRU5ErkJggg==");		
 		
-		std::vector<char> test(code64.begin(), code64.end());
+	void decode(const pp::Var & pictureData,int size) {
+		if(pictureData.is_object()){
+			log("Im an array buffer!");
+		} else {
+			log("or not..");
+		}
 		
-		log(code64);
-											 
+		pp::VarArrayBuffer picArray(pictureData);			
+		std::vector<uint8_t> test(0);
+		
+		char* data = static_cast<char*>(picArray.Map());
+		uint32_t byte_length = picArray.ByteLength();
+
+		//for (uint32_t i = 0; i < byte_length; i++) {
+			//test.push_back(data[1]);
+		//}
+		
+		char buff[10000];
+		sprintf(buff,"size?: %i %i",byte_length, size);
+		log(std::string(buff));
+		/*
 		if(test.empty()){
 			log("hmm");
 		} else {
 			char buff[10000];
-			sprintf(buff,"fact: %i",test.size());
+			sprintf(buff,"fact: %x %x %x %x %x %x %x %x %x %x\n%x %x %x %x %x %x %x %x %x %x\n%x %x %x %x %x %x %x %x %x %x\n%x %x",test[0], test[1], test[2], test[3], test[4], test[5], test[6], test[7], test[8], test[9], test[10], test[11], test[12], test[13], test[14], test[15], test[16], test[17], test[18], test[19], test[20], test[21], test[22], test[23], test[24], test[25], test[26], test[27], test[28], test[29], test[30], test[31]);
+			log(std::string(buff));
+		} */
+		
+		/*									 
+		if(test.empty()){
+			log("hmm");
+		} else {
+			char buff[10000];
+			sprintf(buff,"fact: %x %x %x %x %x %x %x %x %x %x\n%x %x %x %x %x %x %x %x %x %x\n%x %x %x %x %x %x %x %x %x %x\n%x %x",test[0], test[1], test[2], test[3], test[4], test[5], test[6], test[7], test[8], test[9], test[10], test[11], test[12], test[13], test[14], test[15], test[16], test[17], test[18], test[19], test[20], test[21], test[22], test[23], test[24], test[25], test[26], test[27], test[28], test[29], test[30], test[31]);
 			log(std::string(buff));
 		}
 	
@@ -113,11 +133,7 @@ class VitileegoCVEngine : public pp::Instance {
 			} else {
 				log("sample image successfully loaded");
 			}
-
-		//pp::VarArray pic(picture);
-		/*char buff[10000];
-		sprintf(buff,"test %x | %i",picture[2],test.size());
-		log(std::string(buff));*/
+*/
 		
 	}
 	
